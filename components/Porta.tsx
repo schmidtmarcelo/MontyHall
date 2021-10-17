07aeecf7be2e474097a1b8c9f23f5b1ab17bfcf4
Marcelo Schmidt <gmmarceloo@gmail.com>
2021-10-17 01:47:28 -0300

    add typescript

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,13 +1,19 @@
 import styles from "../styles/Porta.module.css"
+import PortaModel from "../model/porta"
 
-export default function Porta(props) {
-    const selecionada = props.selecionada ? styles.selecionada : ''
+interface PortaPros {
+    porta: PortaModel
+}
+
+export default function Porta(props: PortaPros) {
+    const { porta } = props
+    const selecionada = porta.selecionada ? styles.selecionada : ''
 
     return (
         <div className={styles.area}>
             <div className={`${styles.estrutura} ${selecionada}`}>
                 <div className={styles.porta}>
-                    <div className={styles.numero}>3</div>
+                    <div className={styles.numero}>{porta.numero}</div>
                     <div className={styles.macaneta}></div>
                 </div>
             </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import Porta from "../components/Porta";
+import PortaModel from "../model/porta";
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
   return (
     <div style={{display: "flex"}}>
-      <Porta selecionada={true}/>
-      <Porta selecionada={false}/>
+      <Porta porta={p1}/>
     </div>
   )
 }
